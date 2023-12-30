846ce9072ee18af217a30279c220807cbecfceac
Leonardo Severo <leopedroso45@gmail.com>
2023-12-30 05:36:36 -0300

    Manage which task execute in each model

--- sevsd/do_work.py
+++ sevsd/do_work.py
@@ -2,6 +2,14 @@ from sevsd.setup_pipeline import setup_pipeline
 from sevsd.process_task import process_task
 
 def do_work(configs, tasks, path, parallel_exec=True, **kwargs):
+    task_dict = {task["task_id"]: [] for task in tasks}
+    for task in tasks:
+        task_dict[task["task_id"]].append(task)
+
     for config in configs:
-        pipeline = setup_pipeline(config, **kwargs)
-        process_task(tasks, pipeline, path, parallel_exec)
+        pipeline = setup_pipeline(config["model_info"], **kwargs)
+        task_ids = config.get("task_ids", [])
+        for task_id in task_ids:
+            if task_id in task_dict:
+                for task in task_dict[task_id]:
+                    process_task(task["details"], pipeline, path, parallel_exec)
--- tests/test_do_work.py
+++ tests/test_do_work.py
@@ -9,18 +9,23 @@ class TestDoWork(unittest.TestCase):
     @patch('sevsd.do_work.process_task')
     @patch('sevsd.do_work.setup_pipeline')
     def test_do_work(self, mock_setup_pipeline, mock_process_task):
-
         mock_pipeline = MagicMock()
         mock_setup_pipeline.return_value = mock_pipeline
 
-        fake_configs = [('model_path', 'cache_path')]
-        fake_tasks = [("prompt", None, 50, 1, 7.5)]
+        fake_configs = [
+            {"model_info": ("model_path", "cache_path"), "task_ids": [1, 2]}
+        ]
+        fake_tasks = [
+            {"task_id": 1, "details": ("prompt1", None, 50, 1, 7.5)},
+            {"task_id": 2, "details": ("prompt2", None, 30, 2, 8.0)}
+        ]
         fake_path = "test_path"
 
         do_work(fake_configs, fake_tasks, fake_path)
 
-        mock_setup_pipeline.assert_called_once_with(fake_configs[0])
-        mock_process_task.assert_called_once_with(fake_tasks, mock_pipeline, fake_path, True)
+        mock_setup_pipeline.assert_called_once_with(('model_path', 'cache_path'))
+        mock_process_task.assert_any_call(("prompt1", None, 50, 1, 7.5), mock_pipeline, fake_path, True)
+        mock_process_task.assert_any_call(("prompt2", None, 30, 2, 8.0), mock_pipeline, fake_path, True)
 
 if __name__ == '__main__':
     unittest.main()
