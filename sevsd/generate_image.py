3ca84b2b9ba8008c41f60cb4e2a0cd9aea2320c0
Leonardo Severo <leopedroso45@gmail.com>
2024-07-01 19:04:14 -0300

    Add upscaling support

--- sevsd/generate_image.py
+++ sevsd/generate_image.py
@@ -1,12 +1,10 @@
-import numpy as np
 import torch
-from PIL import Image
 from RealESRGAN import RealESRGAN
 from sevsd.setup_device import setup_device
 
 device = setup_device()
 
-def generate_image(job, pipeline, executor, upscaling_factor=4, parallel_exec=True, **kwargs):
+def generate_image(job, pipeline, executor, upscaling_factor=2, parallel_exec=True, **kwargs):
     r"""
     Generates high-resolution images based on textual prompts using the provided Stable Diffusion pipeline and an upscaling model.
 
@@ -22,7 +20,7 @@ def generate_image(job, pipeline, executor, upscaling_factor=4, parallel_exec=Tr
                          - 'inference_steps' (int): Number of inference steps for the pipeline.
                          - 'num_of_exec' (int): Number of images to generate.
                          - 'cfg_scale' (float): The guidance scale for controlling image generation.
-        upscaling_factor (int, optional): The factor by which to upscale the generated images. Defaults to 4.
+        upscaling_factor (int, optional): The factor by which to upscale the generated images. Defaults to 2.
         parallel_exec (bool, optional): If True, generates 'num_of_exec' images in parallel. Otherwise, generates images sequentially. Defaults to True.
         **kwargs: Additional keyword arguments passed to the pipeline function.
 
@@ -57,13 +55,19 @@ def generate_image(job, pipeline, executor, upscaling_factor=4, parallel_exec=Tr
         )
 
     def upscale_image(image, factor):
+        if factor not in [2, 4, 8]:
+            raise ValueError("Upscaling factor must be one of the following: 2, 4, 8")
+        
         model = RealESRGAN(device, scale=factor)
-        model.load_weights('weights/RealESRGAN_x4.pth', download=True)
+        try:
+            model.load_weights('weights/RealESRGAN_x{}.pth'.format(factor), download=True)
+        except Exception as e:
+            print(f"Failed to load RealESRGAN weights: {e}")
+            return None
         
-        pil_image = Image.fromarray(image)
-        upscaled_image = model.predict(pil_image)
-        
-        return np.array(upscaled_image)
+        upscaled_image = model.predict(image)
+    
+        return upscaled_image
 
     try:
         with torch.no_grad():
@@ -73,9 +77,13 @@ def generate_image(job, pipeline, executor, upscaling_factor=4, parallel_exec=Tr
             else:
                 output = execute_pipeline(1)
                 base_images = output["images"]
-            
-            high_res_images = [upscale_image(img, upscaling_factor) for img in base_images]
-            
+            high_res_images = []
+            for img in base_images:
+                upscaled_img = upscale_image(img, upscaling_factor)
+                if upscaled_img is not None:
+                    high_res_images.append(upscaled_img)
+                else:
+                    high_res_images.append(img)
             return high_res_images
     except RuntimeError as e:
         print(f"Runtime error: {e}")
